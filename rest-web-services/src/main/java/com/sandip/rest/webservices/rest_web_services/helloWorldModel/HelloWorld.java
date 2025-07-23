cebb8e65e27c0d16e79b769a54645642c1a24ab9
SandipM9903 <sandipmondal990355@gmail.com>
2025-07-23 10:23:37 +0530

    Second Commit

---
+++ rest-web-services/src/main/java/com/sandip/rest/webservices/rest_web_services/helloWorldModel/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.sandip.rest.webservices.rest_web_services.helloWorldModel;
+
+public class HelloWorld {
+    private String message;
+
+    public HelloWorld(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
